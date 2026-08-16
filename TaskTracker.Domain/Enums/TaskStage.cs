b85b81d1c07ea81de67caeab8fa8c0d56e110137
Kaushik Bhatt <kaushikbhatt.se@gmail.com>
2026-08-16 12:11:42 +0700

    feat(domain): add TaskStage enum
    Fixed 3-state lifecycle: Started -> InProgress -> Finished.

---
+++ TaskTracker.Domain/Enums/TaskStage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTracker.Domain.Enums
+{
+	/// <summary>
+	/// The lifecycle of a TaskItem Started -> InProgress -> Finished. 
+	/// </summary>
+	public enum TaskStage
+	{
+		Started = 0,
+		InProgress = 1,
+		Finished = 2
+	}
+}
